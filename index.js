fc59166ea81101887187e1b02fac9d594dcafcd5
Zhaiyuqing2003 <48752879+Zhaiyuqing2003@users.noreply.github.com>
2022-11-04 00:33:14 -0500

    123132131132133

--- index.js
+++ index.js
@@ -3,11 +3,51 @@ const path = require('path');
 const upload = require('multer')({ dest : 'uploads/' });
 const fs = require('fs');
 
+
 app.post('/api/process_file', upload.single("file"), (req, res) => {
     // get the file path from the request
-    const string = req;
-    console.log(string);
+    // const filePath = path.join(__dirname, req.file.path);
+    // console.log(filePath);
+    // console.log(req.file);
+    // // transform the file
+    let data;
+    try {
+        data = transformBinaryFile(req.file.path);
+    } catch (e) {
+        console.log(e);
+        return res.status(500).send("Error processing file");
+    }
+
+    // console.log(data);
+    console.log("Data Sent")
+
+    // make it CORS friendly
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // send the data back to the client
+    res.send({ data });
 });
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
+
+
+const transformBinaryFile = (name) => {
+    // read file "FALL.TXT", into a buffer
+    const buffer = fs.readFileSync(path.join(__dirname, name));
+    // the file is a binary file, its format is long, float, float, float, repeatedly
+    // so we need to read the buffer 4 bytes at a time, and convert it to a long
+    const array = [];
+
+    array.push(Array(96 / 8).fill(0).map((_, i) => buffer.readDoubleLE(i * 8)));
+
+    // one block is unsigned long + float * 6 = 28 bytes
+    for (let i = 96; i < buffer.length; i += 28) {
+        const time = buffer.readUInt32LE(i);
+        const data = Array(6).fill(0).map((_, j) => buffer.readFloatLE(i + 4 + j * 4));
+        array.push([time, ...data]);
+    }
+
+    return array;
+}
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import Plotly from 'plotly.js-dist'
+
 window.onload = () => {
     const button = document.getElementById('submit');
 
@@ -10,14 +12,87 @@ window.onload = () => {
         const data = new FormData();
         data.append('file', input.files[0]);
 
-        fetch('http://localhost:3000/api/process_file', {
+        const req = new Request('api/process_file', {
             method: 'POST',
-            mode : 'no-cors',
-            body: data,
-        }).then((response) => {
-            return response.text();
-        }).then((data) => {
+            body: data
+        });
+
+        fetch(req).then((response) => {
+            return response.json();
+        }).then(({ data }) => {
             console.log(data);
+            plot(...parse(data), "Acceleration");
         })
     });
+}
+
+const parse = (data) => {
+    const getFrequency = (obj, start, end) => {
+        const time = obj[end][0] - obj[start][0];
+        return (end - start) / time * 1_000_000;
+    }
+
+    const offset = data[0];
+
+    const lsm = {
+        ax : offset[0],
+        ay : offset[1],
+        az : offset[2],
+        axRms : offset[3],
+        ayRms : offset[4],
+        azRms : offset[5],
+    }
+
+    const lis = {
+        ax : offset[6],
+        ay : offset[7],
+        az : offset[8],
+        axRms : offset[9],
+        ayRms : offset[10],
+        azRms : offset[11],
+    }
+
+    const obj = data.slice(1);
+
+    const freq = getFrequency(obj, 0, obj.length - 1);
+
+    return [obj, lsm, lis, freq]
+}
+
+const plot = (obj, lsm, lis, freq, element) => {
+    Plotly.newPlot(element, [{
+        x: obj.map(line => line[0] / 1_000_000),
+        y: obj.map(line => line[1] - lsm.ax),
+        type: 'scatter',
+        name: 'Accel X (LSM)'
+    }, {
+        x: obj.map(line => line[0] / 1_000_000),
+        y: obj.map(line => line[2] - lsm.ay),
+        type: 'scatter',
+        name: 'Accel Y (LSM)'
+    }, {
+        x: obj.map(line => line[0] / 1_000_000),
+        y: obj.map(line => line[3] - lsm.az),
+        type: 'scatter',
+        name : 'Accel Z (LSM)'
+    }, {
+        x: obj.map(line => line[0] / 1_000_000),
+        y: obj.map(line => line[4] - lis.ax),
+        type: 'scatter',
+        name : 'Accel X (LIS)'
+    }, {
+        x: obj.map(line => line[0] / 1_000_000),
+        y: obj.map(line => line[5] - lis.ay),
+        type: 'scatter',
+        name : 'Accel Y (LIS)'
+    }, {
+        x: obj.map(line => line[0] / 1_000_000),
+        y: obj.map(line => line[6] - lis.az),
+        type: 'scatter',
+        name : 'Accel Z (LIS)'
+    }], {
+        xaxis : { title: 'Time (s)' },
+        yaxis : { title: 'Acceleration (m/s^2)' },
+        title : `Acceleration over time (average frequency: ${freq.toFixed(2)}Hz)`
+    })
 }
